98645f285568aafd789bae2b2c3e8c278869357b
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-11-03 08:44:33 -0400

    Added index view and emerald sample.

--- App.rb
+++ App.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
 
+# TODO: make emerald gem for ruby that lets you use
+# emerald directly in ruby style apps.
 class Routes < Sinatra::Base
   get '/' do
-    return "Hello emerald!"
+    erb :index
   end
+
+  # use gsub method to find all instances of
+  # tokens and then replace them with css
+  # classes scoped to them for styles.
 
   run! if app_file == $PROGRAM_NAME
 end
