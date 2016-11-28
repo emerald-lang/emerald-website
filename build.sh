a9f60838ce43bff150642ee75cf3be099ca6bc80
Dave Pagurek <dave@davepagurek.com>
2016-11-27 21:20:09 -0800

    Build as a 'test'

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:emerald-lang/emerald.git
+cd emerald
+gem install bundler
+bundle install
+cd ..
+
+emerald/bin/emerald process index.emr -b
+mv index.html.emr index.html
--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,6 @@
 git config user.name "Circle CI"
 git config user.email "dave@davepagurek.com"
 
-git clone git@github.com:emerald-lang/emerald.git
-cd emerald
-gem install bundler
-bundle install
-cd ..
-
-emerald/bin/emerald process index.emr -b
-mv index.html.emr index.html
-
 message="Deploy commit $(git rev-parse HEAD)"
 echo "Deploying $(git rev-parse HEAD) to master"
 
