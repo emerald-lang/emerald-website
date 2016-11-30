ec2a8ee8df2633dad04f5e1283232aa26908869c
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-11-29 19:55:43 -0500

    Changed sass directory, updated compile script.

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
 # Compile sass and coffeescript here
 coffee -o ./js/ -c coffee/main.coffee
+sass -I css/emerald.css sass/emerald.sass
+sass -I css/syntax.css sass/syntax.sass
